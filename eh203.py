968c867b2823c58c05ff8bf25c121e0e992bf3d3
Antti Laine <aalien@iki.fi>
2017-02-14 20:40:08 +0200

    Add sensor ids for EH-203.

---
+++ eh203.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+SENSORS = [(u'Ulkolämpötila',   18),
+           (u'L1 huone',        21),
+           (u'L1 menovesi',     20),
+           (u'L1 paluuvesi',    23),
+           (u'L2 menovesi',     26),
+           (u'Mittaus 6',       27),
+           (u'LV menovesi',     24),
+           (u'LV kiertovesi',   25),
+           (u'L2 paluuvesi',    33),
+           ]
